e20c8e8dde4a71e1ce668f4ed407a04dc3ca22c5
Bo Li <li.laruku@gmail.com>
2017-02-04 13:01:11 -0500

    Working tests for all timecard controller methods

--- spec/apis/timecards_controller_spec.rb
+++ spec/apis/timecards_controller_spec.rb
@@ -59,7 +59,7 @@ RSpec.describe Api::V1::TimecardsController , :type => :controller do
 
     # ASSUMPTION: in this new context block, there are no timecards
     context "when a non existing id is used" do
-      it "returns 422 Unprocessable Entity" do
+      it "returns 404 Not Found" do
         get :show, params: { id: 1 }
         expect(response).to have_http_status(:not_found)
       end
@@ -70,12 +70,12 @@ RSpec.describe Api::V1::TimecardsController , :type => :controller do
     context "when using valid timecard params" do
       let (:timecard_params) { FactoryGirl.attributes_for :timecard }
 
-      # ASSUMPTION: Successful post request return created object
       it "returns 200" do
         post :create, params: { timecard: timecard_params }
         expect(response).to be_success
       end
 
+      # ASSUMPTION: Successful post request returns created object
       it "returns a timecard" do
         post :create, params: { timecard: timecard_params }
         parsed_json = JSON.parse(response.body)
@@ -87,14 +87,74 @@ RSpec.describe Api::V1::TimecardsController , :type => :controller do
       #end
     end
 
-    context "when an unexisting timecard id is used" do
-      it "returns 422 Unprocessable Entity" do
-      end
+    context "when using inavlid timecard params" do
+      pending
+      #it "returns 422 Unprocessable Entity" do
+      #end
     end
 
     context "when using invalid timecard params" do
       # TODO: Model validations to fail params
       pending
     end
+  end
+
+  describe "#update" do
+    context "when using an existing timecard" do
+      context "when using valid params" do
+        let (:timecard) { FactoryGirl.create :timecard }
+        let (:timecard_params) {{
+          :username => "otherusername",
+          :occurrence => Date.new
+        }}
+
+        it "returns 200 OK" do
+          put :update, params: { id: timecard, timecard: timecard_params }
+          expect(response).to be_success
+        end
+
+        it "returns json" do
+          put :update, params: { id: timecard, timecard: timecard_params }
+          parsed_json = JSON.parse(response.body)
+          expect(parsed_json).to_not be_nil
+        end
+      end
+      
+      context "when using invalid params" do
+        pending
+      end
+    end
+
+    context "when using no timecard" do
+      let (:timecard_params) {{
+        :username => "otherusername",
+        :occurrence => Date.new
+      }}
+
+      it "returns 422 Unprocessable Entity" do
+        put :update, params: { id: 1, timecard: timecard_params }
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+
+  describe "#destroy" do
+    context "when using an existing timecard" do
+      let (:timecard) { FactoryGirl.create :timecard }
+
+      it "returns 200 OK" do
+        delete :destroy, params: { id: timecard }
+        expect(response).to be_success
+      end
+    end
+
+    context "when using a timecard id that doesn't exist" do
+      it "returns 422 Unprocessable Entity" do
+        delete :destroy, params: { id: 1 }
+
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+    
   end
 end  
